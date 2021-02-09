ef487834785f0d804d18b65035657b65564368ba
Aaron Hoffman <aaronhoffman1999@gmail.com>
2021-02-09 18:27:12 +0100

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Aaron Hoffman on 09/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
